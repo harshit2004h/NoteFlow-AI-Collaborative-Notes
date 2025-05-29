8e1bd47dc5b675b5d26d87c22722f63fccbaa041
harshit2004h <harshitsrivastava2004h@gmail.com>
2025-05-30 01:43:35 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "NoteFlow - AI-Powered Note Workspace",
+  description: "Noteflow is an AI-enhanced Notion alternative that helps you write, organize, and collaborate faster. Smart notes, docs, tasks, and powerful AI built into one seamless workspace.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
