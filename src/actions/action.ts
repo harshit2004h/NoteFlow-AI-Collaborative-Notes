10ec92f5a3b0e8eb35ebf934a9e0dca7e1229de2
harshit2004h <harshitsrivastava2004h@gmail.com>
2025-06-22 20:06:09 +0530

    user avatars done

--- src/actions/action.ts
+++ src/actions/action.ts
@@ -80,3 +80,22 @@ export async function InviteUserToDocument(roomId: string, email: string) {
     return { success: false, error: "Failed to invite user" };
   }
 }
+
+export async function removeUserFromDocument(roomId: string, userId: string) {
+  auth.protect();
+  console.log("Removing user from document:", roomId, "-> ", userId);
+
+  try {
+    await adminDb
+      .collection("users")
+      .doc(userId)
+      .collection("rooms")
+      .doc(roomId)
+      .delete();
+
+    return { success: true };
+  } catch (error) {
+    console.error("Error removing user from document:", error);
+    return { success: false, error: "Failed to remove user" };
+  }
+}
--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -15,6 +15,8 @@ import Editor from "./Editor";
 import useOwner from "@/lib/useOwner";
 import DeleteDocument from "./DeleteDocument";
 import InviteUser from "./InviteUser";
+import ManageUsers from "./ManageUsers";
+import Avatars from "./Avatars";
 
 function Document({ id }: { id: string }) {
   const [data, loading, error] = useDocumentData(doc(db, "documents", id));
@@ -60,9 +62,12 @@ function Document({ id }: { id: string }) {
         </form>
       </div>
 
-      <div>
+      <div className="flex max-w-6xl mx-auto justify-between pb-5">
         {/* Manage Users */}
+        <ManageUsers />
+
         {/* Avatars */}
+        <Avatars />
       </div>
 
       <hr className="pb-10" />
